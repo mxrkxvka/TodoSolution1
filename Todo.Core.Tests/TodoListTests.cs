e9fa4fcceb91c7a1587eb77949c7e1abf876a2aa
mxrkxvka <mrmarckis@gmail.com>
2025-11-10 16:36:01 +0700

    Добавьте файлы проекта.

---
+++ Todo.Core.Tests/TodoListTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Core.Tests
+{
+    public class TodoListTests
+    {
+        [Fact]
+        public void Add_IncreasesCount()
+        {
+            var list = new TodoList();
+            list.Add(" task ");
+            Assert.Equal(1, list.Count);
+            Assert.Equal("task", list.Items.First().Title);
+        }
+        [Fact]
+        public void Remove_ById_Works()
+        {
+            var list = new TodoList();
+            var item = list.Add("a");
+            Assert.True(list.Remove(item.Id));
+            Assert.Equal(0, list.Count);
+        }
+        [Fact]
+        public void Find_ReturnsMatches()
+        {
+            var list = new TodoList();
+            list.Add("Buy milk");
+            list.Add("Read book");
+            var found = list.Find("buy").ToList();
+            Assert.Single(found);
+            Assert.Equal("Buy milk", found[0].Title);
+        }
+    }
+}
+
